10e0b60372d70320d58bf36fd715137143a2b2f4
Mihir Jain <63667062+mixh@users.noreply.github.com>
2024-09-04 21:15:31 -0400

    first commit

--- medical_backend/models/Medicine.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const medicineSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-});
-
-const Medicine = mongoose.model("Medicine", medicineSchema);
-
-module.exports = Medicine;
